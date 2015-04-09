134cd842000c6c77c2ca14a1ac3d8277dde59578
Tomas <tomasdepriede@gmail.com>
2015-04-09 00:26:36 -0300

    Added normal api of register/unregister

--- Server/AppEngine/src/main/java/ar/com/ironsoft/marroccl/web/app/modules/devices/guice/DeviceModule.java
+++ Server/AppEngine/src/main/java/ar/com/ironsoft/marroccl/web/app/modules/devices/guice/DeviceModule.java
@@ -2,8 +2,12 @@ package ar.com.ironsoft.marroccl.web.app.modules.devices.guice;
 
 import java.util.LinkedList;
 
+import javax.servlet.http.HttpServlet;
+
 import ar.com.ironsoft.marroccl.web.app.modules.devices.endpoints.DevicesEndpointApi;
 import ar.com.ironsoft.marroccl.web.app.modules.devices.model.Device;
+import ar.com.ironsoft.marroccl.web.app.modules.devices.servlets.DeviceRegistrationServlet;
+import ar.com.ironsoft.marroccl.web.app.modules.devices.servlets.DeviceUnregistrationServlet;
 import ar.com.ironsoft.marroccl.web.guice.GuiceServletsModule;
 
 public class DeviceModule extends GuiceServletsModule {
@@ -13,6 +17,16 @@ public class DeviceModule extends GuiceServletsModule {
         LinkedList<Class<?>> registeredClasses = new LinkedList<>();
         //
         registeredClasses.add(Device.class);
+        //
+        return registeredClasses;
+    }
+
+    @Override
+    public LinkedList<Class<? extends HttpServlet>> getServletClasses() {
+        LinkedList<Class<? extends HttpServlet>> registeredClasses = new LinkedList<>();
+        //
+        registeredClasses.add(DeviceRegistrationServlet.class);
+        registeredClasses.add(DeviceUnregistrationServlet.class);
         //
         return registeredClasses;
     }
---
+++ Server/AppEngine/src/main/java/ar/com/ironsoft/marroccl/web/app/modules/devices/servlets/DeviceRegistrationServlet.java
@@ -0,0 +1,51 @@
+package ar.com.ironsoft.marroccl.web.app.modules.devices.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ar.com.ironsoft.marroccl.web.app.modules.devices.daos.DeviceDao;
+import ar.com.ironsoft.marroccl.web.app.modules.devices.model.Device;
+import ar.com.ironsoft.marroccl.web.core.servlets.BaseServlet;
+import ar.com.ironsoft.marroccl.web.guice.base.BasePath;
+import ar.com.ironsoft.marroccl.web.guice.base.RelativePath;
+
+import com.google.common.base.Preconditions;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * @author Tomas de Priede
+ */
+@Singleton
+@BasePath("/devices/")
+@RelativePath("register")
+public class DeviceRegistrationServlet extends BaseServlet {
+
+    private DeviceDao deviceDao;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String deviceId = String.valueOf(req.getParameter("deviceId"));
+        Preconditions.checkNotNull(deviceId);
+        //
+        Device device = new Device(deviceId);
+        deviceDao.save(device);
+        //
+        setSuccess(resp);
+    }
+
+    @Inject
+    public void setDeviceDao(DeviceDao deviceDao) {
+        this.deviceDao = deviceDao;
+    }
+}
