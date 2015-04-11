76a7759f9abbc9917fc42df8b229bcd164573696
Gabriel Villoldo <gvilloldo@mailexternos.com.ar>
2015-04-11 04:55:48 -0300

    score board

--- Mobile/MarrocCLAndroid/app/src/main/java/ar/com/ironsoft/marrocclandroid/activities/EventActivity.java
+++ Mobile/MarrocCLAndroid/app/src/main/java/ar/com/ironsoft/marrocclandroid/activities/EventActivity.java
@@ -14,7 +14,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.google.gson.Gson;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import ar.com.ironsoft.marrocclandroid.R;
+import ar.com.ironsoft.marrocclandroid.adapters.GameListAdapter;
+import ar.com.ironsoft.marrocclandroid.domain.GameItem;
 import ar.com.ironsoft.marrocclandroid.domain.PushMessage;
 
 
@@ -45,6 +57,7 @@ public class EventActivity extends BaseActionBarActivity {
         }
         setUI();
         playVideo();
+        getScoreBoard();
     }
 
     private Boolean processPushMessageFromIntent() {
@@ -84,8 +97,45 @@ public class EventActivity extends BaseActionBarActivity {
                 bigIcon.setImageResource(R.drawable.red_card_big);
                 playerIcon.setImageResource(R.drawable.red_card);
                 break;
+            default:
+                // Put something
+                break;
         }
         ((TextView)findViewById(R.id.list_item_main_event_time)).setText(pushMessage.getMinutes() + ":" + pushMessage.getSeconds() + "''");
+    }
+
+    private void getScoreBoard() {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://marroccl-909.appspot.com/game/get?gameId=" + pushMessage.getGameId() + "&minute=" + pushMessage.getMinutes() + "&second=" + pushMessage.getSeconds())
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Gson gson = new Gson();
+
+                final GameItem gameItem = gson.fromJson(response.body().string(), GameItem.class);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageLoader.getInstance().displayImage(gameItem.homeTeamLink, (ImageView)findViewById(R.id.score_board_image_rival_home));
+                        ImageLoader.getInstance().displayImage(gameItem.awayTeamLink, (ImageView)findViewById(R.id.score_board_image_rival_away));
+
+                        ((TextView)findViewById(R.id.score_board_home_score)).setText(gameItem.homeTeamScore.toString());
+                        ((TextView)findViewById(R.id.score_board_away_score)).setText(gameItem.awayTeamScore.toString());
+                    }
+                });
+            }
+        });
     }
 
     private void playVideo() {
