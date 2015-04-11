d85ce209b0aea8fc1b1f19cae0f0a322cb2dc391
Tomas <tomasdepriede@gmail.com>
2015-04-11 11:54:37 -0300

    Push to refresh status of game in lists

--- Server/AppEngine/src/main/java/ar/com/ironsoft/marroccl/web/app/modules/game/servlet/GameListServlet.java
+++ Server/AppEngine/src/main/java/ar/com/ironsoft/marroccl/web/app/modules/game/servlet/GameListServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import ar.com.ironsoft.marroccl.web.app.modules.game.daos.GameDao;
 import ar.com.ironsoft.marroccl.web.app.modules.game.model.Game;
+import ar.com.ironsoft.marroccl.web.app.modules.game.services.GameService;
 import ar.com.ironsoft.marroccl.web.core.model.paging.PagingResult;
 import ar.com.ironsoft.marroccl.web.core.servlets.BaseServlet;
 import ar.com.ironsoft.marroccl.web.guice.base.BasePath;
@@ -26,14 +27,29 @@ import com.google.inject.Singleton;
 public class GameListServlet extends BaseServlet {
 
     private GameDao gameDao;
+    private GameService gameService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException, ServletException {
         PagingResult<Game> pagingResult = gameDao.getPageOfAll(Game.class, 100,
                 null);
+        //
+        for (Game game : pagingResult.getResultList()) {
+            game.refreshCurrentTime();
+            int minute = game.getCurrentMinute();
+            int second = game.getCurrentSecond();
+            game = gameService.getGameWithCurrentScore(game, minute, second);
+            gameDao.save(game);
+        }
+        //
         String json = new Gson().toJson(pagingResult.getResultList());
         setJsonResponse(resp, json);
+    }
+
+    @Inject
+    public void setGameService(GameService gameService) {
+        this.gameService = gameService;
     }
 
     @Inject
